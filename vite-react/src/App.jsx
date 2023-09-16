b69a784fd153d3336e99c7c77f91393a1b34503a
Surendra Kumar <surendra874874@gmail.com>
2023-09-16 16:07:26 +0530

    first

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Name2 from "./Name2"
+
+function App() {
+
+  return (
+    <>
+    <Name2 />
+    <h1>react with vite | surendra</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite-react/src/Name2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Name2() {
+  return (
+    <div>
+      <h3>piyush</h3>
+    </div>
+  )
+}
