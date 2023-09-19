311240799ff2c11aea01205cac408f88394759ae
Surendra Kumar <surendra874874@gmail.com>
2023-09-19 17:12:12 +0530

    remaining 3rd

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -2,12 +2,17 @@ import Name2 from "./Name2"
 
 function App() {
 
+  const username = "surendra"
+
   return (
     <>
     <Name2 />
-    <h1>react with vite | surendra</h1>
+    <h1>react with vite {username}</h1>
     </>
   )
 }
 
 export default App
+
+// {username} > evaluated expression that stores the final
+// outcome of js 
