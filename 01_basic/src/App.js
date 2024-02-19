266b41b60cb910f91d3c7c0ec4c17b9b9650931e
Surendra Kumar <surendra874874@gmail.com>
2024-02-19 12:09:04 +0530

    before router

---
+++ 01_basic/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Name2 from './Name2'
+
+export default function App() {
+
+  return (
+    <div>
+      <Name2 />
+      <h1>surendra</h1>
+    </div>
+  )
+}
---
+++ 01_basic/src/Name2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Name2() {
+  return (
+    <>
+    <h2>piyush</h2>
+      
+    </>
+  )
+}
